c148fa7dd3864858ce4a5a37c1e66fcbd5760429
Opemipo Aikomo <opemipoaikomo@gmail.com>
2015-08-04 22:52:33 +0100

    Setup base gem files

---
+++ lib/ng/bank/parser.rb
@@ -0,0 +1,9 @@
+require "ng/bank/parser/version"
+
+module Ng
+  module Bank
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ng/bank/parser/version.rb
@@ -0,0 +1,7 @@
+module Ng
+  module Bank
+    module Parser
+      VERSION = "0.1.0"
+    end
+  end
+end
