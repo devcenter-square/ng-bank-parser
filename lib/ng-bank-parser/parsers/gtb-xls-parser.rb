8a3f38db9e62c4d7b609096ee0ca6a28e23dd7e2
Opemipo Aikomo <opemipoaikomo@gmail.com>
2015-08-05 00:43:59 +0100

    Each Parser must return valid object

--- lib/ng-bank-parser/parsers/gtb-xls-parser.rb
+++ lib/ng-bank-parser/parsers/gtb-xls-parser.rb
@@ -1,7 +1,14 @@
 module NgBankParser
 	class GtbXls
-		def parse
-			return {}
+		def self.parse
+			return {
+				bank_name: "Guaranty Trust Bank",
+				account_number: 01234567,
+				account_name: 'Aikomo Onaopemipo',
+				from_date: Date.today,
+				to_date: Date.today,
+				transactions: []
+			}
 		end
 	end
 end
--- spec/parsers_spec.rb
+++ spec/parsers_spec.rb
@@ -5,26 +5,53 @@ describe "Parsers" do
 
 	it { is_expected.to be_a_kind_of(Array) }
 	it { is_expected.not_to be_empty }
+end
 
 
-	describe "Every Parser" do
-		it "is unique" do
-			expect(subject.length).to equal(subject.uniq.length)
+describe "Every Parser" do
+	subject { $Parsers }
+
+	it "is unique" do
+		expect(subject.length).to equal(subject.uniq.length)
+	end
+	it "has valid keys" do
+		subject.each do |parser|
+			expect(parser).to match({
+				:key => an_instance_of(String),
+				:name => an_instance_of(String),
+				:format => an_instance_of(String),
+				:valid => an_instance_of(String),
+				:invalid => an_instance_of(String),
+				:empty => an_instance_of(String)
+        	})
 		end
-		it "has valid keys" do
-			subject.each do |parser|
-				expect(parser).to be_a_kind_of(Hash)
-				expect(parser.keys).to contain_exactly(:key, :name, :format, :valid, :invalid, :empty)
-			end
+	end
+	it "has parser file" do
+		subject.each do |parser|
+			filename = parser[:key] + "-" + parser[:format] + "-parser.rb"
+			path = "lib/ng-bank-parser/parsers/" + filename
+			
+			expect(File).to exist(path), "Didnt find #{filename} in the parsers folder"
+			expect(File).to exist(path), "Didnt find #{filename} in the parsers folder"
 		end
-		it "has parser file" do
-			subject.each do |parser|
-				filename = parser[:key] + "-" + parser[:format] + "-parser.rb"
-				path = "lib/ng-bank-parser/parsers/" + filename
-				
-				expect(File).to exist(path), "Didnt find #{filename} in the parsers folder"
-				expect(File).to exist(path), "Didnt find #{filename} in the parsers folder"
-			end
+	end
+end
+
+$Parsers.each do |parser|
+	describe parser[:key].capitalize + " " + parser[:format].capitalize + " " +  "Parser" do
+		class_name= parser[:key].capitalize + parser[:format].capitalize;
+		class_object = NgBankParser.const_get(class_name)
+		response = class_object.parse()
+
+		it "returns valid object" do
+			expect(response).to match({
+				:bank_name => an_instance_of(String),
+				:account_number => an_instance_of(Fixnum),
+				:account_name => an_instance_of(String),
+				:from_date => an_instance_of(Date),
+				:to_date => an_instance_of(Date),
+				:transactions => an_instance_of(Array)
+        	})
 		end
 	end
 end
