a1e74a04723362c0f80b5ccb3cc93a2445f7283a
Oluro Olaoluwa <ollaollu@gmail.com>
2015-08-06 23:32:38 +0100

    removed use of array in iterate_me method and used the hash value to fetch extensions

--- lib/ng-bank-parser/router.rb
+++ lib/ng-bank-parser/router.rb
@@ -5,11 +5,12 @@ module NgBankParser
     #The holder for our supported file extensions
     @file_extension_array = []
     @filename = "okay.xls"
+    @bank_name = "gtb"
 
     #this calls the required methods needed to get the bank name and extension
     #and uses it to pick which of the bank parsers to use
     def self.router_delegator
-      iterate_me($Banks, "gtb")
+      iterate_me($Banks, @bank_name)
       parser_picker(@filename)
     end
 
@@ -20,24 +21,20 @@ module NgBankParser
     #and see if the file extension is available for that bank
     #it's result is to fill up the @file_extension_array with the supported bank extensions
     def self.iterate_me(bank_stash, bank_name)
-      temporary_iterate_holder = []
-
       value = bank_stash.index {|x| x[:key] == bank_name}
-      temporary_iterate_holder << bank_stash[value]
-      @file_extension_array += temporary_iterate_holder.map {|e| e[:parsers].map { |x| x[:extensions] } }
+      temporary_iterate_holder = bank_stash[value]
+     @file_extension_array += temporary_iterate_holder[:parsers].map {|e| e[:extensions]}
     end
 
     #this uses the extension in the file of the uploaded payload to check support in @file_extension_array
     #and goes on to pick the parser to use for the operation
     def self.parser_picker(filename)
       extension_name = File.extname(filename).delete('.')
-      @file_extension_array.map do |f|
-        if f.reduce(:concat).include?(extension_name)
+        if @file_extension_array.reduce(:concat).include?(extension_name)
            "Hello"
          else
            "Bye"
          end 
-      end
     end
 
     puts router_delegator
