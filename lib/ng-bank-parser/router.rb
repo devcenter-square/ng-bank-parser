11b901837717a168519f8ca042aebd21f6e42bbf
Oluro Olaoluwa <ollaollu@gmail.com>
2015-08-08 12:21:16 +0100

    code finalised but tests yet to be written

--- lib/ng-bank-parser/router.rb
+++ lib/ng-bank-parser/router.rb
@@ -1,43 +1,51 @@
 require './banks'
+require './parsers/gtb-excel-parser.rb'
 
 module NgBankParser
   class Router
+    #Bank hash
+    @bank_stash = $Banks
     #The holder for our supported file extensions
     @file_extension_array = []
-    @filename = "okay.xls"
-    @bank_name = "gtb"
-
-    #this calls the required methods needed to get the bank name and extension
-    #and uses it to pick which of the bank parsers to use
-    def self.router_delegator
-      iterate_me($Banks, @bank_name)
-      parser_picker(@filename)
-    end
-
+    #The variable that holds the location index for a searched bank in the Bank hash
+    @value
+    
 
     private
     
     #this takes our bank parser along with the bank name supplied from the payload so as to compare
     #and see if the file extension is available for that bank
     #it's result is to fill up the @file_extension_array with the supported bank extensions
-    def self.iterate_me(bank_stash, bank_name)
-      value = bank_stash.index {|x| x[:key] == bank_name}
-      temporary_iterate_holder = bank_stash[value]
-     @file_extension_array += temporary_iterate_holder[:parsers].map {|e| e[:extensions]}
+    def self.router(bank_name, filename)
+      @value = @bank_stash.index {|x| x[:key] == bank_name}
+      
+      if @value.nil?
+        return {status: 0, message: "Your bank is not yet supported"}
+      else
+        @file_extension_array += @bank_stash[@value][:parsers].map {|e| e[:extensions]}
+
+        parser_picker(filename)
+      end      
     end
 
     #this uses the extension in the file of the uploaded payload to check support in @file_extension_array
     #and goes on to pick the parser to use for the operation
     def self.parser_picker(filename)
       extension_name = File.extname(filename).delete('.')
-        if @file_extension_array.reduce(:concat).include?(extension_name)
-           "Hello"
-         else
-           "Bye"
-         end 
+      if @file_extension_array.reduce(:concat).include?(extension_name)
+          ng_bank_parsers(filename)
+      else
+          return {status: 0, message:"The '.#{extension_name}' file format is not supported for your bank."}
+      end         
     end
 
-    puts router_delegator
+    #Builds the the parser to use from the name and file format of the uploaded file
+    def self.ng_bank_parsers(filename)
+      key = @bank_stash[@value][:key].capitalize
+      format = @bank_stash[@value][:parsers].map { |e| e[:format].capitalize }
+      class_name = key + format.reduce(:concat)
+      NgBankParser.const_get(class_name).parse(filename)
+    end
 
   end
 end
